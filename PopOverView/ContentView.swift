9214779c6671dfcd7966a2bfdc91fae297b2bb46
Rob Evans <robevans@microsoft.com>
2022-04-23 20:30:02 -0400

    added popoverview package

--- PopOverView/ContentView.swift
+++ PopOverView/ContentView.swift
@@ -5,47 +5,25 @@
 //  Created by robevans on 4/22/22.
 //
 
+import Popovers
 import UIKit
 import SwiftUI
 
 struct ContentView: View {
-    @State var open = false
-    @State var popoverSize = CGSize(width: 300, height: 300)
-    @State var positionX: CGFloat = 0
-    @State var positionY: CGFloat = 0
 
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
     ]
 
+
     var body: some View {
         GeometryReader { geo in
             ScrollView {
                 LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(0 ..< 15) { item in
+                    ForEach(0 ..< 35) { item in
                         VStack {
-                            WithPopover(
-                                showPopover: $open,
-                                popoverSize: popoverSize,
-                                arrowDirections: [.up],
-                                popoverPosition: CGRect(x: 0, y: 0, width: 0, height: 0), // placeholder values
-                                content: {
-                                    // The view you want to anchor your popover to.
-                                    SomeView(geo: geo,
-                                             open: $open,
-                                             positionY: $positionY)
-                                },
-                                popoverContent: {
-                                    VStack {
-                                        Button(action: { self.popoverSize = CGSize(width: 300, height: 600)}) {
-                                            Text("Increase size")
-                                        }
-                                        Button(action: { self.open = false}) {
-                                            Text("Close")
-                                        }
-                                    }
-                                })
+                            SomeView(text: "item: \(item)")
                         }
                     }
                 }
@@ -55,139 +33,56 @@ struct ContentView: View {
 }
 
 struct SomeView: View {
-    let geo: GeometryProxy
-
-    @Binding var open: Bool
-    @Binding var positionY: CGFloat
+    let text: String
+    @State var open = false
 
     var body: some View {
         VStack {
-            Button(action: {
-                self.open.toggle()
-                positionY = geo.frame(in: .named("OuterV")).midY
-            }) {
-                Text("Tap me - \(positionY)")
-                    .coordinateSpace(name: "OuterV")
-            }
-            .padding()
-        }
-    }
-}
-
-struct WithPopover<Content: View, PopoverContent: View>: View {
-
-    @Binding var showPopover: Bool
-    var popoverSize: CGSize? = nil
-    var arrowDirections: UIPopoverArrowDirection = [.down]
-    var popoverPosition: CGRect = CGRect()
-    let content: () -> Content
-    let popoverContent: () -> PopoverContent
-
-    var body: some View {
-        content()
-            .background(
-                Wrapper(showPopover: $showPopover, arrowDirections: arrowDirections, popoverSize: popoverSize, popoverPosition: popoverPosition, popoverContent: popoverContent)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            )
-    }
-
-    struct Wrapper<PopoverContent: View> : UIViewControllerRepresentable {
-
-        @Binding var showPopover: Bool
-        var arrowDirections: UIPopoverArrowDirection
-        let popoverSize: CGSize?
-        let popoverPosition: CGRect
-        let popoverContent: () -> PopoverContent
-
-        func makeUIViewController(context: UIViewControllerRepresentableContext<Wrapper<PopoverContent>>) -> WrapperViewController<PopoverContent> {
-            return WrapperViewController(
-                popoverSize: popoverSize,
-                popoverPosition: popoverPosition,
-                permittedArrowDirections: arrowDirections,
-                popoverContent: popoverContent) {
-                    self.showPopover = false
+            Text(text)
+                .onTapGesture {
+                    open = true
                 }
-        }
+                .popover(
+                    present: $open,
+                    attributes: {
+                        $0.sourceFrameInset.top = 35
+                        $0.presentation.animation = .spring()
+                        $0.position = .absolute(
+                            originAnchor: .bottom,
+                            popoverAnchor: .topLeft
+                        )
+                    }
+                ) {
+                    VStack {
+                        Text("Hi, I'm a popover. - \(text)")
+                            .padding()
+                    }
+                    .frame(width: 350, height: 350, alignment:  .center)
+                    .background(.white)
+                    .cornerRadius(46)
+                    .shadow(color: .secondary, radius: 14, x: 2, y: 4)
+                }
+            .padding()
 
-        func updateUIViewController(_ uiViewController: WrapperViewController<PopoverContent>,
-                                    context: UIViewControllerRepresentableContext<Wrapper<PopoverContent>>) {
-            uiViewController.updateSize(popoverSize)
+/*
 
-            if showPopover {
-                uiViewController.showPopover()
-            }
-            else {
-                uiViewController.hidePopover()
-            }
-        }
-    }
+This is an example of with an arrow, but we need to do some work to determine arrow location with lazygrid
 
-    class WrapperViewController<PopoverContent: View>: UIViewController, UIPopoverPresentationControllerDelegate {
+ Templates.Container(
+     arrowSide: .bottom(.mostClockwise),
+     backgroundColor: .green
+ ) {
+     Text("This is a pretty standard-looking popover with an arrow.")
+         .foregroundColor(.white)
+ }
+ .frame(maxWidth: 300)
 
-        var popoverSize: CGSize?
-        var popoverPosition: CGRect?
-        let permittedArrowDirections: UIPopoverArrowDirection
-        let popoverContent: () -> PopoverContent
-        let onDismiss: () -> Void
+ */
 
-        var popoverVC: UIViewController?
-
-        required init?(coder: NSCoder) { fatalError("") }
-        init(popoverSize: CGSize?,
-             popoverPosition: CGRect,
-             permittedArrowDirections: UIPopoverArrowDirection,
-             popoverContent: @escaping () -> PopoverContent,
-             onDismiss: @escaping() -> Void) {
-            self.popoverSize = popoverSize
-            self.permittedArrowDirections = permittedArrowDirections
-            self.popoverContent = popoverContent
-            self.onDismiss = onDismiss
-            super.init(nibName: nil, bundle: nil)
-        }
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-
-        func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-            return .none // this is what forces popovers on iPhone
-        }
-
-        func showPopover() {
-            guard popoverVC == nil else { return }
-            let vc = UIHostingController(rootView: popoverContent())
-            if let size = popoverSize { vc.preferredContentSize = size }
-            vc.modalPresentationStyle = UIModalPresentationStyle.popover
-            if let popover = vc.popoverPresentationController {
-                popover.sourceView = view
-                popover.permittedArrowDirections = self.permittedArrowDirections
-                popover.sourceRect = self.popoverPosition ?? CGRect(x: 0, y: 0, width: 0, height: 0)
-                popover.sourceView = self.view
-                popover.delegate = self
-            }
-            popoverVC = vc
-            self.present(vc, animated: true, completion: nil)
-        }
-
-        func hidePopover() {
-            guard let vc = popoverVC, !vc.isBeingDismissed else { return }
-            vc.dismiss(animated: true, completion: nil)
-            popoverVC = nil
-        }
-
-        func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
-            popoverVC = nil
-            self.onDismiss()
-        }
-
-        func updateSize(_ size: CGSize?) {
-            self.popoverSize = size
-            if let vc = popoverVC, let size = size {
-                vc.preferredContentSize = size
-            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
